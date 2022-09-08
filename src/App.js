0fcac25d77f1e99765f66480b5cbd4ece26bedde
JanithSandaru <janithsandaru999@gmail.com>
2022-09-08 17:14:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import SearchWeather from "./component/SearchWeather";
 
-
 function App() {
   return (
     <div className="App">
--- src/component/SearchWeather.jsx
+++ src/component/SearchWeather.jsx
@@ -1,25 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Button } from 'semantic-ui-react';
 
-const SearchWeather = () => {
-    return (
-        <div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-4">
-                        <div className="card text-bg-dark">
-                            <img src="https://wallpaper.dog/large/176458.jpg" width={"400px"} className="card-img" alt="ad" />
-                            <div className="card-img-overlay">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a
-                                    natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text">Last updated 3 mins ago</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+class SearchWeather extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello</h1>
+                <Button>Click Here</Button>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default SearchWeather;
